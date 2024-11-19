3f3f0fa98dfd77828ed6b4aadd603c7e1ad0b31e
rigellin73 <rigellin73@gmail.com>
2024-11-19 15:59:59 +0100

    use grid to show current weather

--- src/App.py
+++ src/App.py
@@ -3,7 +3,6 @@ import weatherapi
 from weatherapi.rest import ApiException
 from pprint import pprint
 import yaml
-#from datetime import date
 import tkinter as tk
 
 import requests
@@ -17,6 +16,19 @@ def get_location_city():
     ip_address = get_ip()
     response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
     return response.get("city")
+
+def show_weather(window, api_response):
+    pprint(api_response)
+
+    # create result as a table
+    for index, key in enumerate(api_response):
+        key_entry = tk.Entry(window, width=20, fg='blue', font=('Arial', 16, 'bold'))
+        value_entry = tk.Entry(window, width=20, fg='blue', font=('Arial', 16, 'bold'))
+        key_entry.grid(row=index, column=0)
+        value_entry.grid(row=index, column=1)
+        key_entry.insert(tk.END, key)
+        value_entry.insert(tk.END, api_response[key])
+
 
 # Read config file
 config = yaml.safe_load(open("../config/config.yml"))
@@ -32,23 +44,19 @@ api_instance = weatherapi.APIsApi(weatherapi.ApiClient(configuration))
 
 # get location
 
-ip_location_city = get_location_city() # str | Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name. Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to learn more.
-
-# get current date
-#dt = date.today().strftime("%y-%m-%d") # date | Date on or after 1st Jan, 2015 in yyyy-MM-dd format
-
-def show_weather(api_response):
-    pprint(api_response)
-    window = tk.Tk()
-    label = tk.Label(text=api_response)
-    label.pack()
-    window.mainloop()
+#ip_location_city = get_location_city() # str | Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name.
+ip_location_city = "Malmo"
 
 try:
-    # Astronomy API
-    #api_astronomy_response = api_instance.astronomy(ip_location_city, dt)
     api_realtime_response = api_instance.realtime_weather(ip_location_city)
-    show_weather(api_realtime_response["current"])
+
+    # create a root window.
+    main_window = tk.Tk()
+    main_window.title("Current weather")
+    # Define the size of the window.
+    main_window.geometry("500x750")
+    show_weather(main_window, api_realtime_response["current"])
+    main_window.mainloop()
 
 except ApiException as e:
     print("Exception when calling APIsApi->realtime_weather: %s\n" % e)
